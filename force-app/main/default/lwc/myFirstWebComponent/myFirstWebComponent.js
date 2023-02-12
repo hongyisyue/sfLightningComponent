5846d78ddd6ba9b01972f30ff5a66d223d58c854
hong <35433456+hongyisyue@users.noreply.github.com>
2023-02-11 18:14:38 -0800

    Selected opportunity works but not witht the search component

--- force-app/main/default/lwc/myFirstWebComponent/myFirstWebComponent.js
+++ force-app/main/default/lwc/myFirstWebComponent/myFirstWebComponent.js
@@ -1,23 +1,23 @@
 import { api, LightningElement} from 'lwc';
 
 // Oppurtunity objects
-import OPPO_OBJECT from '@salesforce/schema/Opportunity';
-import OPPO_NAME from '@salesforce/schema/Opportunity.Name';
-import OPPO_AMOUNT from '@salesforce/schema/Opportunity.Amount';
+// import OPPO_OBJECT from '@salesforce/schema/Opportunity';
+// import OPPO_NAME from '@salesforce/schema/Opportunity.Name';
+// import OPPO_AMOUNT from '@salesforce/schema/Opportunity.Amount';
 
 export default class MyFirstWebComponent extends LightningElement {
 
     // The current record ID
     @api recordId;
-    fieldsToCreate = ['Name','Rating','Phone','Industry']
-    fields        = ['Name'];
-
-    // @api recordId;
-    oppoObj = OPPO_OBJECT;
-    oppoFields = [
-        OPPO_NAME,
-        OPPO_AMOUNT
-    ];
+    objName = 'Opportunity'
+    displayFields = 
+    [
+        'Name',
+        'Annual_SPED_Budget__c',
+        'CurrencyIsoCode',
+        'School_Year__c',
+        'CloseDate'
+    ]
 
 
     // The input change will affect the look up for the related record
