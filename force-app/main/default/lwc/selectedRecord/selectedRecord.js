28e8b5af2eea681630c196c1c777986864363cd8
hong <35433456+hongyisyue@users.noreply.github.com>
2023-04-03 18:21:36 -0700

    add profile link to detail view

--- force-app/main/default/lwc/selectedRecord/selectedRecord.js
+++ force-app/main/default/lwc/selectedRecord/selectedRecord.js
@@ -1,23 +1,29 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 export default class SelectedRecord extends LightningElement {
+    @api obj;
+    @api recordid;
+    @api fields;
+
+    @track recordUrl
+    
+    connectedCallback() {
+        this.recordUrl = '/lightning/r/Account/' + this.recordid + '/view';
+    }
+    
     handleRemove = (event) => {
         event.preventDefault();
         const closeEvent = new CustomEvent('close', {
-            bubbles    : true,
-            composed   : true,
-            cancelable : true,
+            bubbles: true,
+            composed: true,
+            cancelable: true,
             detail: {
-                data : {
-                    record     : undefined,
-                    recordId   : undefined
+                data: {
+                    record: undefined,
+                    recordId: undefined
                 }
             }
         });
         this.dispatchEvent(closeEvent);
     }
-
-    @api obj;
-    @api recordid;
-    @api fields;
 }
